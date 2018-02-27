832c8ae1bb5a41d2428ce4c35fcb33ebd88708f9
Min Kim <mk4555@nyu.edu>
2018-02-27 18:08:28 +0000

    automatically backed up by learn

--- spec/features/visitors/about_page_spec.rb
+++ spec/features/visitors/about_page_spec.rb
@@ -3,7 +3,7 @@
 # #   I want to visit an 'about' page
 # #   So I can learn more about the website
 # feature 'About page' do
-# 
+#
 #   # Scenario: Visit the 'about' page
 #   #   Given I am a visitor
 #   #   When I visit the 'about' page
@@ -12,5 +12,5 @@
 #     visit '/about'
 #     expect(page).to have_content 'About the Website'
 #   end
-# 
+#
 # end
