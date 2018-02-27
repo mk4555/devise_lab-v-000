9c730b22af29217b8cfe123a655d1f269d71fc55
Min Kim <mk4555@nyu.edu>
2018-02-27 18:08:30 +0000

    automatically backed up by learn

--- spec/features/visitors/home_page_spec.rb
+++ spec/features/visitors/home_page_spec.rb
@@ -3,7 +3,7 @@
 # #   I want to visit a home page
 # #   So I can learn more about the website
 # feature 'Home page' do
-# 
+#
 #   # Scenario: Visit the home page
 #   #   Given I am a visitor
 #   #   When I visit the home page
@@ -12,5 +12,5 @@
 #     visit root_path
 #     expect(page).to have_content 'Welcome'
 #   end
-# 
+#
 # end
