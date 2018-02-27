25f6411ad977cbb43dc0a76fa1ca733f799b49b3
Min Kim <mk4555@nyu.edu>
2018-02-27 17:45:44 +0000

    automatically backed up by learn

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -13,7 +13,7 @@ Devise.setup do |config|
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
   config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
-  config.omniauth :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
+  config.omniauth :facebook, ENV['FACEBOOK_KEY'], ENV['bb59381fc781bb490b449dfeb31bece2']
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
 
